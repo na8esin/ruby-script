6a5b8444713e860faf192ddbfa5df7e7c4d5fa2f
Takayuki Watanabe <watanabe@sikmi.com>
2022-12-28 11:24:39 +0900

    group_byのパターン

--- array/inject.rb
+++ array/inject.rb
@@ -8,3 +8,8 @@ users.push(User.new(68))
 
 injected = users.inject({}) { |result, u| result.merge({ u.id => u }) }
 p injected[33] # <struct User id=33>
+
+# ここまでやっといてなんだけど、group byで行けるんじゃね
+# {2=>[#<struct User id=2>], 33=>[#<struct User id=33>], 45=>[#<struct User id=45>], 68=>[#<struct User id=68>]}
+grouped = users.group_by(&:id)
+p grouped[33].first # と思ったけど、firstがうっとうしい。
