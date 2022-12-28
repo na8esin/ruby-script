7198bc03dcc7ccad6f84cad4ee9e54641aaf83cf
Takayuki Watanabe <watanabe@sikmi.com>
2022-12-28 11:00:45 +0900

    フィールド定義と初期化を簡素にしたい

---
+++ array/inject.rb
@@ -0,0 +1,2 @@
+u1 = User.new { |u| u.id = 12 }
+p u1.id
---
+++ struct/static_struct.rb
@@ -0,0 +1,18 @@
+Customer = Struct.new(:name, :address)
+
+#<struct Customer name="Dave", address="123 Main">
+p Customer.new("Dave", "123 Main")
+
+# Customerと表示される
+p Customer.new("Dave", "123 Main").class
+
+# この書き方もできるけど、Structの方が短い
+class User
+  attr_accessor :id
+
+  def initialize
+    yield self
+  end
+end
+
+p User.new {|u| u.id = 12}.id # 12
