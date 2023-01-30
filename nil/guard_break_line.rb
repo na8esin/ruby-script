5cfbdd76d0c87dffa5e526d7d1f45f39af8b1275
Takayuki Watanabe <watanabe@sikmi.com>
2023-01-30 10:55:04 +0900

    nil guard break line: メモ化がちゃんとされてるか、コード追加

--- nil/guard_break_line.rb
+++ nil/guard_break_line.rb
@@ -1,10 +1,20 @@
 class User
   ITEMS = [1, 2, 3].freeze
+  attr_accessor :item_id
 
-  def item(item_id:)
+  def item
     @item ||= ITEMS
-              .find{ |i| i == item_id }
+              .find { |i| i == item_id }
   end
 end
 
-p User.new.item(item_id: 2)
+p u = User.new
+u.item_id = 3
+
+# メモ化しておく
+p u.item
+
+u.item_id = nil
+
+# メモ化されたものが呼び出される
+p u.item # 3
