eaa780461793b5ac59d3d3128684cafaedc69cc2
Takayuki Watanabe <watanabe@sikmi.com>
2023-05-22 12:04:23 +0900

    多重代入

---
+++ hash/shorthand.rb
@@ -0,0 +1,13 @@
+def sample
+  { success: 1, fail: 2 }
+end
+
+success, failure = sample
+
+p success # {:success=>1, :fail=>2}
+p failure # nil
+
+success, failure = sample.values
+
+p success # 1
+p failure # 2
