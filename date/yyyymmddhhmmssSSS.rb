650b0f9bfb5addc2aaeba9533872a68c3330e0f8
Takayuki Watanabe <watanabe@sikmi.com>
2023-06-05 16:23:35 +0900

    strftime: 月日

--- date/yyyymmddhhmmssSSS.rb
+++
@@ -1,2 +0,0 @@
-# 20220503101100856
-Time.current.strftime('%Y%m%d%H%M%S%L')
---
+++ date_and_time/strftime.rb
@@ -0,0 +1,7 @@
+require 'date'
+require 'active_support/all'
+
+# 20220503101100856
+p Time.current.strftime('%Y%m%d%H%M%S%L')
+
+p Time.current.strftime('%m月%d日')
