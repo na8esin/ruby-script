21e3e0482812d0377503b47eafd584775023d317
Takayuki Watanabe <watanabe@sikmi.com>
2025-09-09 17:48:19 +0900

    後置if, 論理積

---
+++ if_clauses/logical_and.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+a == 1 &&
+  b == 1
---
+++ if_clauses/post_if2.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+a = 1
+b = 1
+
+raise StandardError if a == 1 &&
+                       b == 1
+
+p 'hello'
