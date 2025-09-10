fdb57a8eb21dcd2d54bd818c412a05299082e5b6
Takayuki Watanabe <watanabe@sikmi.com>
2025-09-10 13:30:51 +0900

    aws get put

---
+++ aws/put_presigned_url.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'aws-sdk-s3'
+
+signer = Aws::S3::Presigner.new
+url = signer.presigned_url(
+  :get_object,
+  bucket: ENV.fetch('BUCKET'),
+  key: 'sample.png', # ← 省略できない
+)
+
+p url
