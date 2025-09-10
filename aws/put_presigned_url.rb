dea597d7a0eb65f1bcb6d9ac3398139bcd7c59b8
Takayuki Watanabe <watanabe@sikmi.com>
2025-09-10 15:39:14 +0900

    put presigned_url

--- aws/put_presigned_url.rb
+++ aws/put_presigned_url.rb
@@ -1,12 +1,40 @@
 # frozen_string_literal: true
 
+# 実行方法
+# AWS_PROFILE=??? BUCKET=??? ruby aws/put_presigned_url.rb
+
 require 'aws-sdk-s3'
 
 signer = Aws::S3::Presigner.new
-url = signer.presigned_url(
-  :get_object,
+presigned_url = signer.presigned_url(
+  :put_object,
   bucket: ENV.fetch('BUCKET'),
   key: 'sample.png', # ← 省略できない
 )
 
-p url
+p presigned_url
+
+url = URI.parse(presigned_url)
+
+# 画像をアップロードする
+require 'net/http'
+require 'uri'
+require 'openssl'
+
+File.open("#{__dir__}/sample.png", 'rb') do |f|
+  # URLのquery parametersも含めてリクエストURIを作成
+  request_uri = url.path + (url.query ? "?#{url.query}" : '')
+  req = Net::HTTP::Put.new(request_uri)
+  req.body = f.read
+  req.content_type = 'image/png'
+
+  # HTTPSの設定
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+  res = http.request(req)
+
+  puts "Response code: #{res.code}"
+  puts "Response body: #{res.body}" if res.body
+end
